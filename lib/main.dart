fa180a9aed4c5da293760f657df9ca261c60a818
Krista Balling <kristaballing@Mac.attlocal.net>
2025-03-17 13:32:14 -0400

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
        body: GradientContainer(
-        const Color.fromARGB(255, 59, 5, 147),
-      const Color.fromARGB(255, 183, 145, 245)), 
+         Color.fromARGB(255, 59, 5, 147),
+         Color.fromARGB(255, 183, 145, 245)), 
       ),
     ),
  );
